dc14f15a677555713f6b4f4b915043c30daaa899
Emiliano Mendoza <eemmiii96@gmail.com>
2022-04-09 20:33:56 -0300

    primer commit

---
+++ CustomersApi/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomersApi.Dtos;
+
+namespace CustomersApi.Controllers
+{   
+    //[controller] se traduce automaticamente a customers
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+
+        [HttpGet("{id}")]
+        public async Task<Customer> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<Customer> CreateCustomer(CreateCustomer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<Customer> UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomersApi/Dtos/CreateCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersApi.Dtos
+{
+    public class CreateCustomer
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string LastName { get; set; }
+
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}", ErrorMessage= "El email es invalido")]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
